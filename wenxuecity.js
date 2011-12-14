e5267afad0b52eff0ee6f30a88a1a1475da65f45
Zhonghai Zuo <zhonghai.zuo@gmail.com>
2011-12-14 10:51:36 -0500

    1. added support for other forums 2. fixed the timezone issue

--- wenxuecity.js
+++ wenxuecity.js
@@ -7,7 +7,8 @@ wenxuecity.forums = {
   news:   [wenxuecity.url + "/news", wenxuecity.url + "/news/"],
   gossip: [wenxuecity.url + "/news/gossip", wenxuecity.url], 
   ent:    [wenxuecity.url + "/news/ent", wenxuecity.url],
-  joke:   ['joke']
+  // joke:   ["bbs.wenxuecity.com/joke/?reply=0", "bbs.wenxuecity.com/joke/"]
+  bbs:   ["bbs.wenxuecity.com/", "bbs.wenxuecity.com/"]
 };
 
 wenxuecity.load = function(forums, date){
@@ -19,14 +20,19 @@ wenxuecity.load = function(forums, date){
     // add a new tab and make it active(current)
     gBrowser.selectedTab = gBrowser.addTab(wenxuecity.url);
 
-    forums = forums.split('|')
+    forums = forums.toString().split(' ')
     for(var i = 0; i < forums.length; i++){
-      forumHomePage(wenxuecity.forums[forums[i]][0], wenxuecity.forums[forums[i]][1]);
+      if(forums[i] == 'news' || forums[i] == 'gossip' || forums[i] == 'ent'){
+        newsHomePage(wenxuecity.forums[forums[i]][0], wenxuecity.forums[forums[i]][1]);
+      }else{
+        forumHomePage(wenxuecity.forums['bbs'][0] + forums[i] + "/?reply=0", wenxuecity.forums['bbs'][1] + forums[i] + "/");
+      }
     }
   }
 
-  function forumHomePage(forumHomeUrl, baseUrl){
-    gBrowser.getBrowserForTab(gBrowser.addTab(forumHomeUrl + '/1')).addEventListener("load", 
+  // TODO: pagination
+  function newsHomePage(newsUrl, baseUrl){
+    gBrowser.getBrowserForTab(gBrowser.addTab(newsUrl + '/1')).addEventListener("load", 
         function processPostList(event){
           // alert(event.originalTarget);
           var es = event.originalTarget.getElementsByTagName("a");
@@ -43,10 +49,33 @@ wenxuecity.load = function(forums, date){
         }, true);
   }
 
+
+  // TODO: pagination
+  function forumHomePage(forumHomeUrl, baseUrl){
+    gBrowser.getBrowserForTab(gBrowser.addTab(forumHomeUrl)).addEventListener("load", 
+        function processPostList(event){
+          // alert(event.originalTarget);
+          var es = event.originalTarget.getElementsByTagName("small");
+          var s = date.split('/');
+          s = parseInt(s[1], 10).toString() + "/" + parseInt(s[2], 10).toString() + "/" + (parseInt(s[0], 10) - 2000).toString();
+          // var reg = new RegExp(/12\/7\/11/);
+          var reg = new RegExp(s);
+          for(var j = 0; j < es.length; j++){
+            var h = es[j].innerHTML; // parentNode.childNodes[0].innerHTML;
+            if(h.match(reg)){
+              gBrowser.getBrowserForTab(gBrowser.addTab(baseUrl + es[j].parentNode.getAttribute("class").split('-')[1] + '.html')).addEventListener("load", processPost, true);
+              // TODO debug only, remove me
+              // break;
+            }
+          }
+        }, true);
+  }
+
   // clean up the page remove the ads, header, footer...
   function processPost(event){
-    // var doc = postBrowser.contentDocument;
     var doc = event.originalTarget;
+
+    // followings are from news
     removeElement(doc.getElementById('topnav'));
     removeElement(doc.getElementById('headwrapper'));
     removeElement(doc.getElementById('navwrapper'));
@@ -54,7 +83,17 @@ wenxuecity.load = function(forums, date){
     removeElement(doc.getElementById('bookmark'));
     removeElement(doc.getElementById('subwrapper'));
     removeElement(doc.getElementById('sidewidget'));
-    removeElement(doc.getElementsByClassName('sponsored')[0]);
+    removeElement(castToArray(doc.getElementsByClassName('sponsored')));
+
+    // followings are from forums 
+    removeElement(doc.getElementById('topboard'));
+    removeElement(doc.getElementById('header'));
+    removeElement(doc.getElementById('menubar'));
+    removeElement(doc.getElementById('headerNavigation'));
+    removeElement(doc.getElementById('postreply'));
+    removeElement(castToArray(doc.getElementsByClassName('headerNavigation')));
+
+    // in commen
     removeElement(doc.getElementById('footer'));
   }
 
@@ -62,7 +101,15 @@ wenxuecity.load = function(forums, date){
     if(el == null)
       return false;
 
-    el.parentNode.removeChild(el); 
+    if(el instanceof Array){
+      for(var i = 0; i < el.length; i++) 
+        el[i].parentNode.removeChild(el[i]);
+    }else
+      el.parentNode.removeChild(el); 
+  }
+
+  function castToArray(elements){
+    return Array.filter(elements, function(e){return e});
   }
 
 }
@@ -74,12 +121,16 @@ commands.addUserCommand(
   function(args){
     if (!args['-forum'] || args["-forum"].length == 0) {
         // looking for global vars in your .vimperatorrc
-        var forum = typeof liberator.globalVariables.wenxuecity_forum == "undefined" ? "news|gossip|ent" : liberator.globalVariables.wenxuecity_forum;
+        var forum = typeof liberator.globalVariables.wenxuecity_forum == "undefined" ? "news gossip ent" : liberator.globalVariables.wenxuecity_forum;
     } else {
         forum = args["-forum"];
     }
 
     var date = new Date();
+    // adjust the timezone diff
+    var tzDiff = date.getTimezoneOffset() / 60;
+    date.setHours(date.getHours() - tzDiff);
+
     if (!args["-date"] || args["-date"].length == 0) {
       date = date.toISOString().split('T')[0].replace(/-/g, '/');
     }else{
@@ -95,7 +146,19 @@ commands.addUserCommand(
   {
     options: [
       [["-date", "-d"], commands.OPTION_LIST, null, [["today", "today"], ["yesterday", "yesterday"]], false],
-      [["-forum", "-f"], commands.OPTION_LIST, null, null, true]
+      [["-forum", "-f"], commands.OPTION_LIST, null, [
+                                                      ["joke", "难得一笑"], 
+                                                      ["tv", "影视人生"], 
+                                                      ["cooking", "私房小菜"], 
+                                                      ["znjy", "子女教育"], 
+                                                      ["music", "音乐快递"],
+                                                      ["origin", "海外原创"],
+                                                      ["photography", "摄影沙龙"],
+                                                      ["news", "焦点新闻"],
+                                                      ["ent", "娱乐新闻"],
+                                                      ["gossip", "生活新闻"],
+                                                      ["ZOMG", "You spend too much time on wenxuecity...(let me know what you want :-)"]
+                                                     ], true]
     ]
   }
 );
